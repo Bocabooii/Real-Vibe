ae9d97992f6dcd0b6e1fefb3f93c5b77708314b0
Alexis Bocanegra <alexisbocanegra14@gmail.com>
2023-09-25 17:06:05 -0500

    Nice looking start page

--- Real-Vibe/src/components/Home.jsx
+++ Real-Vibe/src/components/Home.jsx
@@ -6,11 +6,19 @@ const Home = () => {
         <div name="home" className="bg-gradient-to-b from-black to-gray-700 w-full h-screen mx-auto">
             <div className='flex flex-col items-center justify-center h-full'>
                 <h1 className='text-6xl font-marker m-2 bg-gradient-to-r from-pink-600 via-pink-500 to-white text-transparent bg-clip-text'>REAL VIBE</h1>
-                <p className='text-xl text-white font-gaming'>This is a guessing game of music</p>
+                <div className='flex flex-col justify-center items-center text-xl text-white font-gaming space-y-5'>
+                  <p>Test your knowledge in our music trivia game!</p>
+                  <p>How to play:</p>
+                  <p>1. Read the random lyrics from recent top 100 songs</p>
+                  <p>2. Click the artist photo that matches the lyrics</p>
+                  <p>3. Save your score!</p>
+                </div> 
+                <div className='flex flex-col mx-auto p-2 m-5 bg-pink-500 rounded-md shadow-md shadow-white'>
+                  <button className='text-white font-marker text-xl'>Start</button>
+                </div>
             </div>
         </div>
     </div>
   )
 }
-
 export default Home
